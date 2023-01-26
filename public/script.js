aeb7ecb673db24c3aa376507040f2e416b41c08c
Ashish Singh <sing.ashish940@gmail.com>
2023-01-26 15:13:56 +0530

    added history, result section in the webpage

--- public/script.js
+++ public/script.js
@@ -1,3 +1,7 @@
+var history = document.getElementById("history");
+
+
 var editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
-    editor.session.setMode("ace/mode/javascript");
+    document.getElementById("editor").style.backgroundColor = '#000000';
+    editor.session.setMode("ace/mode/c_cpp");
