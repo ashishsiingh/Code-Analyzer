aa9ddd3dd5273093a1422500bde9f04254c76c03
Ashish Singh <sing.ashish940@gmail.com>
2023-02-05 22:39:22 +0530

    added submit button & updated the labguage selector

--- public/script.js
+++ public/script.js
@@ -1,5 +1,19 @@
 var history = document.getElementById("history");
 
+const languageSelector = document.querySelector(".language-selector");
+const btn = languageSelector.querySelector(".btn");
+const languageOptions = languageSelector.querySelector(".language-options");
+
+btn.addEventListener("click", function() {
+  languageOptions.style.display = (languageOptions.style.display === "block") ? "none" : "block";
+});
+
+languageOptions.addEventListener("click", function(event) {
+  const selectedLanguage = event.target;
+  btn.textContent = selectedLanguage.textContent;
+  languageOptions.style.display = "none";
+});
+
 
 var editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
